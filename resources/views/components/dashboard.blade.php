6d25264cd544a0309d652607a530525a59e7a6c1
George Okojie Davis <binarygeotech@gmail.com>
2025-02-16 13:37:30 +0100

    Updated simple branding

---
+++ resources/views/components/dashboard.blade.php
@@ -0,0 +1,31 @@
+<div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
+    <x-application-logo class="block h-20 w-auto" />
+
+    <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+        Welcome to your Weather Companion!
+    </h1>
+
+    <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
+        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
+        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
+        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
+        ecosystem to be a breath of fresh air. We hope you love it.
+    </p>
+</div>
+
+<div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+    <div>
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="size-6 stroke-gray-400">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+            </svg>
+            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
+                Authentication
+            </h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started with what matters most: building your application.
+        </p>
+    </div>
+</div>
